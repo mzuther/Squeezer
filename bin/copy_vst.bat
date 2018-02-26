ed82f946a9d3c8155bf3737ffee378b1a6f296eb
Martin Zuther <code@mzuther.de>
2018-02-26 22:35:50 +0100

    updated VST copy script

--- bin/copy_vst.bat
+++ bin/copy_vst.bat
@@ -1,44 +1,55 @@
 @echo off
+setlocal
 
-@setlocal
-@set rsync_path=E:\Documents\System\Tools\rsync\bin
-@set rsync_cmd=%rsync_path%\rsync.exe --archive
+set rsync_path=E:\Documents\System\Tools\rsync\bin
+set rsync_cmd=%rsync_path%\rsync.exe --archive
 
-@set destination=/cygdrive/d/Vst/32bit/radix/
-@echo %destination%
-@%rsync_cmd% --delete "vst/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Mono).dll"         "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Stereo).dll"       "%destination%"
+set vst2_32=/cygdrive/c/Program Files (x86)/Steinberg/VSTPlugins/radix/
+set vst3_32=/cygdrive/c/Program Files (x86)/Common Files/VST3/radix/
+set vst2_64=/cygdrive/c/Program Files/Steinberg/VSTPlugins/radix/
+set vst3_64=/cygdrive/c/Program Files/Common Files/VST3/radix/
 
-@set destination=/cygdrive/d/Vst/Categories/Dynamics/Compressor/
-@echo %destination%
-@%rsync_cmd% --delete "vst/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Mono).dll"         "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Stereo).dll"       "%destination%"
+call :CopyVst ^
+ "%vst2_32%" ^
+ "vst/Squeezer (Mono).dll" ^
+ "vst/Squeezer (Stereo).dll" ^
+ "vst/squeezer"
 
-@set destination=/cygdrive/d/Plugins/VST2 (32 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "vst/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Mono).dll"         "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Stereo).dll"       "%destination%"
+call :CopyVst ^
+ "%vst3_32%" ^
+ "vst/Squeezer (Mono).vst3" ^
+ "vst/Squeezer (Stereo).vst3" ^
+ "vst/squeezer"
 
-@set destination=/cygdrive/d/Plugins/VST2 (64 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "vst/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Mono x64).dll"     "%destination%"
-@%rsync_cmd%          "vst/Squeezer (Stereo x64).dll"   "%destination%"
+call :CopyVst ^
+ "%vst2_64%" ^
+ "vst/Squeezer (Mono x64).dll" ^
+ "vst/Squeezer (Stereo x64).dll" ^
+ "vst/squeezer"
 
-@set destination=/cygdrive/d/Plugins/VST3 (32 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "vst3/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst3/Squeezer (Mono).vst3"        "%destination%"
-@%rsync_cmd%          "vst3/Squeezer (Stereo).vst3"      "%destination%"
+call :CopyVst ^
+ "%vst3_64%" ^
+ "vst/Squeezer (Mono x64).vst3" ^
+ "vst/Squeezer (Stereo x64).vst3" ^
+ "vst/squeezer"
 
-@set destination=/cygdrive/d/Plugins/VST3 (64 bit)/radix/
-@echo %destination%
-@%rsync_cmd% --delete "vst3/squeezer"                    "%destination%"
-@%rsync_cmd%          "vst3/Squeezer (Mono x64).vst3"    "%destination%"
-@%rsync_cmd%          "vst3/Squeezer (Stereo x64).vst3"  "%destination%"
+pause
+exit /b %errorlevel%
 
-@echo.
-@pause
+
+:CopyVst
+set destination=%1
+set dll_1=%2
+set dll_2=%3
+set aux_dir=%4
+
+echo %destination%
+
+if not exist %dll_1% exit /b 1
+if not exist %dll_2% exit /b 2
+
+%rsync_cmd% --delete %aux_dir% %destination%
+%rsync_cmd%          %dll_1%   %destination%
+%rsync_cmd%          %dll_2%   %destination%
+
+exit /b 0
