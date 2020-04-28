8333e91ab9f942ee024cc1e49a45da42c05ba5e9
Martin Zuther <code@mzuther.de>
2020-04-28 18:58:05 +0200

    add Emacs Flycheck support

---
+++ Source/frut/.dir-locals.el
@@ -0,0 +1,16 @@
+(
+ (nil . ((eval . (let* ((relative-root "../../")
+                        (root (concat (projectile-project-root) relative-root))
+                        (language-standard "c++14")
+                        (include-path (list (concat root "JuceLibraryCode/")
+                                            (concat root "libraries/juce/modules/")
+                                            (concat root "Source/frut/")
+                                            (concat root "libraries/"))))
+
+                   (setq-local flycheck-clang-language-standard language-standard)
+                   (setq-local flycheck-clang-include-path include-path)
+
+                   (setq-local flycheck-gcc-language-standard language-standard)
+                   (setq-local flycheck-gcc-include-path include-path)))))
+ (c-mode . ((mode . c++)))
+ )
