fd2d9f5dd0829c61566c7839ba0fa5814ab46896
Martin Zuther <code@mzuther.de>
2020-05-02 15:07:00 +0200

    automate creation of formatting scripts

--- Source/format_code.bat
+++ Source/format_code.bat
@@ -1,29 +1,36 @@
 @echo off
 
-@rem ---------------------------------------------------------------------------
-@rem
-@rem  Squeezer
-@rem  ========
-@rem  Flexible general-purpose audio compressor with a touch of citrus
-@rem
-@rem  Copyright (c) 2013-2020 Martin Zuther (http://www.mzuther.de/)
-@rem
-@rem  This program is free software: you can redistribute it and/or modify
-@rem  it under the terms of the GNU General Public License as published by
-@rem  the Free Software Foundation, either version 3 of the License, or
-@rem  (at your option) any later version.
-@rem
-@rem  This program is distributed in the hope that it will be useful,
-@rem  but WITHOUT ANY WARRANTY; without even the implied warranty of
-@rem  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-@rem  GNU General Public License for more details.
-@rem
-@rem  You should have received a copy of the GNU General Public License
-@rem  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-@rem
-@rem  Thank you for using free software!
-@rem
-@rem ---------------------------------------------------------------------------
+rem  ----------------------------------------------------------------------------
+rem  
+rem  Squeezer
+rem  ========
+rem  Flexible general-purpose audio compressor with a touch of citrus
+rem  
+rem  Copyright (c) 2013-2020 Martin Zuther (http://www.mzuther.de/)
+rem  
+rem  This program is free software: you can redistribute it and/or modify
+rem  it under the terms of the GNU General Public License as published by
+rem  the Free Software Foundation, either version 3 of the License, or
+rem  (at your option) any later version.
+rem  
+rem  This program is distributed in the hope that it will be useful,
+rem  but WITHOUT ANY WARRANTY; without even the implied warranty of
+rem  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+rem  GNU General Public License for more details.
+rem  
+rem  You should have received a copy of the GNU General Public License
+rem  along with this program.  If not, see <http://www.gnu.org/licenses/>.
+rem  
+rem  Thank you for using free software!
+rem  
+rem  ----------------------------------------------------------------------------
+
+
+rem  ############################################################################
+rem
+rem  WARNING: this file is auto-generated, please do not edit!
+rem
+rem  ############################################################################
 
 @AStyle --recursive --exclude="resources" --options=./.astylerc "*.cpp"
 @echo.
--- Source/frut/format_code.sh
+++ Source/frut/format_code.sh
@@ -1,29 +1,36 @@
-#!/bin/bash
+#! /usr/bin/env bash
 
-# ----------------------------------------------------------------------------
-#
+#  ----------------------------------------------------------------------------
+#  
 #  FrutJUCE
 #  ========
 #  Common classes for use with the JUCE library
-#
+#  
 #  Copyright (c) 2010-2020 Martin Zuther (http://www.mzuther.de/)
-#
+#  
 #  This program is free software: you can redistribute it and/or modify
 #  it under the terms of the GNU General Public License as published by
 #  the Free Software Foundation, either version 3 of the License, or
 #  (at your option) any later version.
-#
+#  
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
 #  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 #  GNU General Public License for more details.
-#
+#  
 #  You should have received a copy of the GNU General Public License
 #  along with this program.  If not, see <http://www.gnu.org/licenses/>.
-#
+#  
 #  Thank you for using free software!
+#  
+#  ----------------------------------------------------------------------------
+
+
+###############################################################################
 #
-# ----------------------------------------------------------------------------
+#  WARNING: this file is auto-generated, please do not edit!
+#
+###############################################################################
 
 astyle --recursive --exclude="resources" --options=./.astylerc \
        "*.cpp"
